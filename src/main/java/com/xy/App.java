4e3450e6fbbc270f347be270cf54fc820f9f7943
yuwb <yuwb@xiaoshouyi.com>
2019-04-19 23:34:56 +0800

    GitCloneTest

--- src/main/java/com/xy/App.java
+++ src/main/java/com/xy/App.java
@@ -1,13 +1,21 @@
 package com.xy;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws GitAPIException {
+        Git git = Git.cloneRepository()
+                .setURI("git@github.com:yuweibo/jmeter.git")
+                .setDirectory(new File("/Users/yuweibo/Downloads/git_clone_test/0"))
+                .call();
+        System.out.println(git);
     }
 }
